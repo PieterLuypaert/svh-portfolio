285c10e752ab3d1c74b45aad1799a0a7745ee49d
Pieter Luypaert <pieter.luypaert@telenet.be>
2025-09-10 19:37:31 +0200

    Refactor code structure for improved readability and maintainability

--- src/js/portfolioManager.js
+++ src/js/portfolioManager.js
@@ -145,7 +145,7 @@ export class PortfolioManager {
 
       return `
         <div class="audio-section">
-            <h4>🎵 Luister fragment:</h4>
+            <h4>Luister fragment:</h4>
             <audio controls preload="metadata" class="audio-player">
                 <source src="${audioSrc}" type="audio/mpeg">
                 <source src="${audioSrc.replace(
